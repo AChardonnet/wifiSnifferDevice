4b00b9baf4281ac182140825d2d8201c49d812e2
Antoine CHARDONNET <antoine.chardonnet@centrale-med.fr>
2026-03-03 12:46:06 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,97 @@
+# Projet 3A - Monitoring de la queue du Ru : WiFi Sniffer
+
+## Installation
+
+### Install Kismet
+
+[documentation here](https://www.kismetwireless.net/docs/readme/installing/)
+
+For Debian Trixie (compatible with the most recent versions of RaspberryPi OS) :
+
+It is reccomended to install kismet as suid-root.
+
+```bash
+wget -O - https://www.kismetwireless.net/repos/kismet-release.gpg.key --quiet | gpg --dearmor | sudo tee /usr/share/keyrings/kismet-archive-keyring.gpg >/dev/null
+echo 'deb [signed-by=/usr/share/keyrings/kismet-archive-keyring.gpg] https://www.kismetwireless.net/repos/apt/release/trixie trixie main' | sudo tee /etc/apt/sources.list.d/kismet.list >/dev/null
+sudo apt update
+sudo apt install kismet
+```
+
+If you installed kismet as suid-root you need to join the kismet group :
+
+```bash
+sudo usermod -aG kismet your-user-here
+```
+
+Create a service in `/etc/systemd/system/kismet.service` :
+
+```INI
+[Unit]
+Description=Kismet Wireless Sniffer
+After=network.target
+
+[Service]
+Type=simple
+User=pi
+Group=kismet
+ExecStart=/usr/bin/kismet --no-line-wrap
+Restart=always
+RestartSec=5
+
+[Install]
+WantedBy=multi-user.target
+```
+
+### Configure Kismet
+
+All config overrides are done in `/etc/kismet/kismet_site.conf`.
+
+Create the log folder :
+
+```bash
+sudo mkdir -p /var/log/kismet
+sudo chown your-user-here:kismet /var/log/kismet
+sudo chmod 775 /var/log/kismet
+```
+
+Configure the logs folder :
+
+```conf
+log_prefix=/var/log/kismet/
+```
+
+Configure the network interface(s) used to capture traffic :
+
+```conf
+source=wlan1
+```
+
+Change the log timeout (logs will be kept this long) :
+
+```conf
+kis_log_device_timeout=1800
+kis_log_packet_timeout=1800
+kis_log_alert_timeout=1800
+kis_log_message_timeout=1800
+kis_log_snapshot_timeout=1800
+```
+
+Flag the logs as ephemeral :
+
+```conf
+kis_log_ephemeral_dangerous=true
+```
+
+### Start the Service
+
+```bash
+sudo systemctl daemon-reload
+sudo systemctl enable kismet
+sudo systemctl start kismet
+```
+
+Check the service :
+
+```bash
+sudo systemctl status kismet
+```
---
+++ deviceCounter.go
@@ -0,0 +1,128 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/joho/godotenv"
+)
+
+type Device struct {
+	Type string `json:"kismet.device.base.type"`
+}
+
+type Data struct {
+	NbPeople int    `json:"nb_people"`
+	Source   string `json:"source"`
+}
+
+func countWifiClients(host string, username string, password string, periodSec int) (int, error) {
+	timestamp := time.Now().Unix() - int64(periodSec)
+	url := fmt.Sprintf("http://%s/devices/views/phy-IEEE802.11/last-time/%d/devices.json", host, timestamp)
+
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return 0, err
+	}
+
+	req.SetBasicAuth(username, password)
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return 0, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return 0, fmt.Errorf("API returned status: %s", resp.Status)
+	}
+
+	var devices []Device
+	err = json.NewDecoder(resp.Body).Decode(&devices)
+	if err != nil {
+		return 0, err
+	}
+
+	count := 0
+	for _, d := range devices {
+		if d.Type == "Wi-Fi Client" {
+			count++
+		}
+	}
+
+	return count, nil
+}
+
+func runEachSec(interval time.Duration, function func()) {
+	ticker := time.NewTicker(interval)
+	defer ticker.Stop()
+
+	for range ticker.C {
+		function()
+	}
+}
+
+func runCounter() {
+	t1 := time.Now()
+	kismetUrl := os.Getenv("KISMET_URL")
+	kismetUser := os.Getenv("KISMET_USER")
+	kismetPassword := os.Getenv("KISMET_PASSWORD")
+	count, err := countWifiClients(kismetUrl, kismetUser, kismetPassword, 600)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	fmt.Println("Wi-Fi Clients seen in the last 10 minutes:", count)
+
+	data := Data{
+		NbPeople: count,
+		Source:   "wifi",
+	}
+	sendDataToBack(data)
+	fmt.Printf("Execution took %d sec\n", int64(time.Since(t1).Seconds()))
+}
+
+func sendDataToBack(data Data) {
+	backendUrl := os.Getenv("BACKEND_URL")
+	url := fmt.Sprintf("http://%s/new_data", backendUrl)
+
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		fmt.Println("Error marshalling JSON:", err)
+		return
+	}
+
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonData))
+	if err != nil {
+		fmt.Println("Error creating request:", err)
+		return
+	}
+
+	req.Header.Set("Content-type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println("Error :", err)
+	}
+	defer resp.Body.Close()
+
+	fmt.Println("Status Code:", resp.StatusCode)
+}
+
+func main() {
+	fmt.Println("Loading .env ...")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	fmt.Println("Counter starting")
+	runEachSec(30*time.Second, runCounter)
+}
